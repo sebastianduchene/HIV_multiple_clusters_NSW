a85dbdcb00f053fa1dbc29dffc0b439b5c696820
sebastianduchene <sebastian.duchene6@gmail.com>
2024-08-08 11:08:35 +0200

    updated with empty alignments for testing

--- empty_alignments/deidentify.R
+++ empty_alignments/deidentify.R
@@ -1,12 +1,11 @@
 library(NELSI)
 
-
 fasta_files <- dir(pattern = "fasta")
+
 
 for(i in 1:length(fasta_files)){
     f <- read.dna(fasta_files[i], format = "fasta")
-    rownames(f)
-    rownames_temp <- paste0("seq_", 1:nrow(f), rownames(f))
+    rownames(f) <- paste0("seq_", 1:nrow(f), rownames(f))
     #print(rownames_temp)
     cat("writing", fasta_files[i])
     write.dna(f, file = fasta_files[i], format = "fasta", nbcol = -1, colsep = "")
